c15c285b4cd3bdcf025694531c54a0c6926bb826
kangsanghwan <47845232+kangsanghwan@users.noreply.github.com>
2019-03-25 10:21:35 +0900

    190325

--- radio.php
+++ radio.php
@@ -89,36 +89,36 @@
 			</div>
 
 
-			<h3>Radio button type</h3>
-			<p>Radio button type</p>
+			<h3>Radio button type01</h3>
+			<p>Radio button type01</p>
 			<div class="demo-block">
 				<div class="source">
 					<div class="ui-radio">
-						<span class="ui-radio_button">
+						<span class="ui-radio_button margin-5">
 							<strong>
 								<input type="radio" id="ex_ard01" name="ex02_rd"> 
 								<label for="ex_ard01">Radio01</label>
 							</strong>
 						</span>
-						<span class="ui-radio_button">
+						<span class="ui-radio_button margin-5">
 							<strong>
 								<input type="radio" id="ex_ard02" name="ex02_rd"> 
 								<label for="ex_ard02">Radio02</label> 
 							</strong>		
 						</span>
-						<span class="ui-radio_button">
+						<span class="ui-radio_button margin-5">
 							<strong>
 								<input type="radio" id="ex_ard03" name="ex02_rd"> 
 								<label for="ex_ard03">Radio03</label> 
 							</strong>
 						</span>
-						<span class="ui-radio_button">
+						<span class="ui-radio_button margin-5">
 							<strong>
 								<input type="radio" id="ex_ard04" name="ex02_rd"> 
 								<label for="ex_ard04">Radio04</label> 
 							</strong>
 						</span>
-						<span class="ui-radio_button">
+						<span class="ui-radio_button margin-5">
 							<strong>
 								<input type="radio" id="ex_ard05" name="ex02_rd"> 
 								<label for="ex_ard05">Radio05</label> 
@@ -128,20 +128,26 @@
 
 
 					<div class="ui-radio ui-radio_small">
-						<input type="radio" id="ex_asrd01" name="ex02_srd"> 
-						<label for="ex_asrd01">Radio01</label> 
-
-						<input type="radio" id="ex_asrd02" name="ex02_srd"> 
-						<label for="ex_asrd02">Radio02</label> 
-
-						<input type="radio" id="ex_asrd03" name="ex02_srd"> 
-						<label for="ex_asrd03">Radio03</label> 
-
-						<input type="radio" id="ex_asrd04" name="ex02_srd"> 
-						<label for="ex_asrd04">Radio04</label> 
-
-						<input type="radio" id="ex_asrd05" name="ex02_srd"> 
-						<label for="ex_asrd05">Radio05</label> 
+						<span class="ui-radio_button">
+							<input type="radio" id="ex_asrd01" name="ex02_srd"> 
+							<label for="ex_asrd01">Radio01</label> 	
+						</span>
+						<span class="ui-radio_button">
+							<input type="radio" id="ex_asrd02" name="ex02_srd"> 
+							<label for="ex_asrd02">Radio02</label> 
+						</span>	
+						<span class="ui-radio_button">
+							<input type="radio" id="ex_asrd03" name="ex02_srd"> 
+							<label for="ex_asrd03">Radio03</label> 
+						</span>
+						<span class="ui-radio_button">
+							<input type="radio" id="ex_asrd04" name="ex02_srd"> 
+							<label for="ex_asrd04">Radio04</label> 
+						</span>
+						<span class="ui-radio_button">
+							<input type="radio" id="ex_asrd05" name="ex02_srd"> 
+							<label for="ex_asrd05">Radio05</label> 
+						</span>
 					</div>
 				</div>
 				<script>
@@ -151,41 +157,79 @@
 						radiodBtn.on("click" , function(){
 							if(radiodBtn.find("strong")){
 								$(this).parent().removeClass("active");
-							} else {
+							
 								$(this).parent().addClass("active");
 							}
 						});
 					})
-				</script>
-				<script>
-				 $(function(){
-				  var sBtn = $("ul > li");    //  ul > li 이를 sBtn으로 칭한다. (클릭이벤트는 li에 적용 된다.)
-				  sBtn.find("a").click(function(){    // sBtn에 속해 있는  a 찾아 클릭 하면.
-				   sBtn.removeClass("active");    // sBtn 속에 (active) 클래스를 삭제 한다.
-				   $(this).parent().addClass("active"); // 클릭한 a에 (active)클래스를 넣는다.
-				  })
-				 })
-				</script>
-				<ul>
-
-			     <li><a>1</a></li>
-
-			     <li><a>2</a></li>
-
-			     <li><a>3</a></li>
-
-			     <li><a>4</a></li>
-
-			     <li><a>5</a></li>
-
-			     <li><a>6</a></li>
-
-			   </ul>
-				
+				</script>				
 
 				<div class="meta">
 					<div class="description">
-						기본형 Radio
+						radio type01
+					</div>
+							
+					<div class="highlight">
+					</div>
+				</div>
+				<div class="demo-control fa"></div>
+			</div>
+			
+
+			<h3>Radio button type02</h3>
+			<p>Radio button type02</p>
+			<div class="demo-block">
+				<div class="source">
+					<div class="ui-radio">
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_k01" name="ex_kw"> 
+							<label for="ex_k01">ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label>
+						</div>						
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_k02" name="ex_kw"> 
+							<label for="ex_k02">ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label> 
+						</div>		
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_k03" name="ex_kw"> 
+							<label for="ex_k03">ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label> 
+						</div>
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_k04" name="ex_kw"> 
+							<label for="ex_k04">ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LANDALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label> 
+						</div>
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_k05" name="ex_kw"> 
+							<label for="ex_k05">ALL for LAND</label> 
+						</div>
+					</div>
+					<hr>
+					<div class="ui-radio ui-radio_small">
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_ks01" name="ex_ksw"> 
+							<label for="ex_ks01">ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label>
+						</div>						
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_ks02" name="ex_ksw"> 
+							<label for="ex_ks02">ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label> 
+						</div>		
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_ks03" name="ex_ksw"> 
+							<label for="ex_ks03">ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label> 
+						<div>
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_ks04" name="ex_ksw"> 
+							<label for="ex_ks04">ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LANDALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND ALL for LAND</label> 
+						</div>
+						<div class="margin-top-5 margin-bottom-5">
+							<input type="radio" id="ex_ks05" name="ex_ksw"> 
+							<label for="ex_ks05">ALL for LAND</label> 
+						</div>
+					</div>
+				</div>
+
+				<div class="meta">
+					<div class="description">
+						radio type02
 					</div>
 							
 					<div class="highlight">
